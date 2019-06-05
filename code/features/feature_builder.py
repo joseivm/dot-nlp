16100fe2092c93669629406ff73f44a4ef7b2cd7
Jose Velarde <joseivm@mit.edu>
2019-06-05 12:47:37 -0700

    adding code for 3 separate classifiers

--- code/features/feature_builder.py
+++ code/features/feature_builder.py
@@ -16,7 +16,7 @@ from tqdm import tqdm, trange
 
 from torch.nn import CrossEntropyLoss, MSELoss
 from scipy.stats import pearsonr, spearmanr
-# from sklearn.metrics import matthews_corrcoef, f1_score
+from sklearn.metrics import matthews_corrcoef, f1_score
 
 from pytorch_pretrained_bert.file_utils import PYTORCH_PRETRAINED_BERT_CACHE, WEIGHTS_NAME, CONFIG_NAME
 from pytorch_pretrained_bert.modeling import BertForSequenceClassification, BertConfig
@@ -88,10 +88,11 @@ class DataProcessor(object):
 class DOTProcessor(DataProcessor):
     """Processor for the 1977 DOT data set"""
 
-    def get_train_examples(self, data_dir):
+    def get_train_examples(self, data_dir,code=None):
         """See base class."""
-        # logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
         lines = []
+        code_to_idx = {'data':0,'people':1,'things':2}
+        label_idx = code_to_idx[code]
         with open(data_dir+'/train.csv','r',errors='ignore') as f:
             reader = csv.reader(f)
             for line in reader:
@@ -101,14 +102,16 @@ class DOTProcessor(DataProcessor):
         for (i, line) in enumerate(lines):
             guid = "%s-%s" % (set_type, i)
             text_a = line[2]
-            label = int(line[0][4:7])
-            label = self.to_three_digit(label)
+            label = line[0][4:7]
+            label = label[label_idx] if code is not None else label
+            label = int(label)
             examples.append(InputExample(guid=guid, text_a=text_a, label=label))
         return examples
 
-    def get_dev_examples(self, data_dir):
+    def get_dev_examples(self, data_dir,code=None):
         """See base class."""
         lines = []
+        code_to_idx = {'data':0,'people':1,'things':2}
         with open(data_dir+'/dev.csv','r',errors='ignore') as f:
             reader = csv.reader(f)
             for line in reader:
@@ -118,15 +121,15 @@ class DOTProcessor(DataProcessor):
         for (i, line) in enumerate(lines):
             guid = "%s-%s" % (set_type, i)
             text_a = line[2]
-            label = int(line[0][4:7])
-            label = self.to_three_digit(label)
+            label = line[0][4:7]
+            label = label[label_idx] if code is not None else label
+            label = int(label)
             examples.append(InputExample(guid=guid, text_a=text_a, label=label))
         return examples
 
-    def get_labels(self):
+    def get_labels(self,code=None):
         """See base class."""
-        labels = range(1000)
-        labels = [self.to_three_digit(x) for x in labels]
+        labels = list(range(10)) if code is not None else list(range(1000))
         return labels
 
     def to_three_digit(self,x):
@@ -151,97 +154,83 @@ class DOTProcessor(DataProcessor):
                 InputExample(guid=guid, text_a=text_a, label=label))
         return examples
 
-def convert_examples_to_features(examples, label_list, max_seq_length,
-                                 tokenizer, output_mode):
-    """Loads a data file into a list of `InputBatch`s."""
+    def convert_examples_to_features(self, examples, label_list, max_seq_length,
+                                     tokenizer, output_mode):
+        """Loads a data file into a list of `InputBatch`s."""
 
-    label_map = {label : i for i, label in enumerate(label_list)}
+        label_map = {label : i for i, label in enumerate(label_list)}
 
-    features = []
-    for (ex_index, example) in enumerate(examples):
-        if ex_index % 10000 == 0:
-            logger.info("Writing example %d of %d" % (ex_index, len(examples)))
+        features = []
+        for (ex_index, example) in enumerate(examples):
 
-        tokens_a = tokenizer.tokenize(example.text_a)
+            tokens_a = tokenizer.tokenize(example.text_a)
 
-        tokens_b = None
-        if example.text_b:
-            tokens_b = tokenizer.tokenize(example.text_b)
-            # Modifies `tokens_a` and `tokens_b` in place so that the total
-            # length is less than the specified length.
-            # Account for [CLS], [SEP], [SEP] with "- 3"
-            _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
-        else:
-            # Account for [CLS] and [SEP] with "- 2"
-            if len(tokens_a) > max_seq_length - 2:
-                tokens_a = tokens_a[:(max_seq_length - 2)]
+            tokens_b = None
+            if example.text_b:
+                tokens_b = tokenizer.tokenize(example.text_b)
+                # Modifies `tokens_a` and `tokens_b` in place so that the total
+                # length is less than the specified length.
+                # Account for [CLS], [SEP], [SEP] with "- 3"
+                _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+            else:
+                # Account for [CLS] and [SEP] with "- 2"
+                if len(tokens_a) > max_seq_length - 2:
+                    tokens_a = tokens_a[:(max_seq_length - 2)]
 
-        # The convention in BERT is:
-        # (a) For sequence pairs:
-        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
-        #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
-        # (b) For single sequences:
-        #  tokens:   [CLS] the dog is hairy . [SEP]
-        #  type_ids: 0   0   0   0  0     0 0
-        #
-        # Where "type_ids" are used to indicate whether this is the first
-        # sequence or the second sequence. The embedding vectors for `type=0` and
-        # `type=1` were learned during pre-training and are added to the wordpiece
-        # embedding vector (and position vector). This is not *strictly* necessary
-        # since the [SEP] token unambiguously separates the sequences, but it makes
-        # it easier for the model to learn the concept of sequences.
-        #
-        # For classification tasks, the first vector (corresponding to [CLS]) is
-        # used as as the "sentence vector". Note that this only makes sense because
-        # the entire model is fine-tuned.
-        tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
-        segment_ids = [0] * len(tokens)
+            # The convention in BERT is:
+            # (a) For sequence pairs:
+            #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+            #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+            # (b) For single sequences:
+            #  tokens:   [CLS] the dog is hairy . [SEP]
+            #  type_ids: 0   0   0   0  0     0 0
+            #
+            # Where "type_ids" are used to indicate whether this is the first
+            # sequence or the second sequence. The embedding vectors for `type=0` and
+            # `type=1` were learned during pre-training and are added to the wordpiece
+            # embedding vector (and position vector). This is not *strictly* necessary
+            # since the [SEP] token unambiguously separates the sequences, but it makes
+            # it easier for the model to learn the concept of sequences.
+            #
+            # For classification tasks, the first vector (corresponding to [CLS]) is
+            # used as as the "sentence vector". Note that this only makes sense because
+            # the entire model is fine-tuned.
+            tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
+            segment_ids = [0] * len(tokens)
 
-        if tokens_b:
-            tokens += tokens_b + ["[SEP]"]
-            segment_ids += [1] * (len(tokens_b) + 1)
+            if tokens_b:
+                tokens += tokens_b + ["[SEP]"]
+                segment_ids += [1] * (len(tokens_b) + 1)
 
-        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
-        input_mask = [1] * len(input_ids)
+            # The mask has 1 for real tokens and 0 for padding tokens. Only real
+            # tokens are attended to.
+            input_mask = [1] * len(input_ids)
 
-        # Zero-pad up to the sequence length.
-        padding = [0] * (max_seq_length - len(input_ids))
-        input_ids += padding
-        input_mask += padding
-        segment_ids += padding
+            # Zero-pad up to the sequence length.
+            padding = [0] * (max_seq_length - len(input_ids))
+            input_ids += padding
+            input_mask += padding
+            segment_ids += padding
 
-        assert len(input_ids) == max_seq_length
-        assert len(input_mask) == max_seq_length
-        assert len(segment_ids) == max_seq_length
+            assert len(input_ids) == max_seq_length
+            assert len(input_mask) == max_seq_length
+            assert len(segment_ids) == max_seq_length
 
-        if output_mode == "classification":
-            label_id = label_map[example.label]
-        elif output_mode == "regression":
-            label_id = float(example.label)
-        else:
-            raise KeyError(output_mode)
+            if output_mode == "classification":
+                label_id = label_map[example.label]
+            elif output_mode == "regression":
+                label_id = float(example.label)
+            else:
+                raise KeyError(output_mode)
 
-        if ex_index < 5:
-            logger.info("*** Example ***")
-            logger.info("guid: %s" % (example.guid))
-            logger.info("tokens: %s" % " ".join(
-                    [str(x) for x in tokens]))
-            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-            logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-            logger.info(
-                    "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-            logger.info("label: %s (id = %d)" % (example.label, label_id))
-
-        features.append(
-                InputFeatures(input_ids=input_ids,
-                              input_mask=input_mask,
-                              segment_ids=segment_ids,
-                              label_id=label_id))
-    return features
-
+            features.append(
+                    InputFeatures(input_ids=input_ids,
+                                  input_mask=input_mask,
+                                  segment_ids=segment_ids,
+                                  label_id=label_id))
+        return features
 
 # processor = DOTProcessor
 # output_mode = 'classification'
--- code/models/train_model.py
+++ code/models/train_model.py
@@ -18,6 +18,7 @@ from tqdm import tqdm, trange
 
 from torch.nn import CrossEntropyLoss, MSELoss
 from scipy.stats import pearsonr, spearmanr
+from sklearn.metrics import mean_squared_error
 
 def convert_examples_to_features(examples, label_list, max_seq_length,
                                  tokenizer, output_mode):
@@ -112,149 +113,184 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
 
 def compute_metrics(preds, labels):
     assert len(preds) == len(labels)
-    return acc_and_f1(preds,labels)
+    return acc_and_mse(preds,labels)
 
-def acc_and_f1(preds, labels):
+def acc_and_mse(preds, labels):
     acc = simple_accuracy(preds, labels)
-    return {
-        "acc": acc,
-    }
+    mse = mean_squared_error(preds,labels)
+    return {"acc": acc, "mse": mse}
 
 def simple_accuracy(preds, labels):
     return (preds == labels).mean()
 
+def train_model(code=None):
+    data_dir = "/home/joseivm/dot-nlp/data/1977"
+    output_dir = "/home/joseivm/dot-nlp/models"
+    results_dir = "/home/joseivm/dot-nlp/results"
+    bert_model = 'bert-large-uncased'
+    output_mode = 'classification'
+    max_seq_length = 128
+    learning_rate = 5e-5
+    warmup_proportion = 0.1
+    num_train_epochs = 6
+    output_dir = os.path.join(output_dir,code)
+    results_dir = os.path.join(results_dir,code)
+    processor = fb.DOTProcessor()
+    labels = processor.get_labels(code)
+    num_labels = len(labels)
+    tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=True)
+    train_examples = processor.get_train_examples(data_dir,code)
+    cache_dir = os.path.join(str(PYTORCH_PRETRAINED_BERT_CACHE), 'distributed_{}'.format(-1))
+    model = BertForSequenceClassification.from_pretrained(bert_model,cache_dir=cache_dir,
+                  num_labels=num_labels)
 
-data_dir = "/home/joseivm/dot-nlp/data/1977"
-output_dir = "/home/joseivm/dot-nlp/models"
-processor = fb.DOTProcessor()
-labels = processor.get_labels()
-bert_model = 'bert-large-uncased'
-tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=True)
-train_examples = processor.get_train_examples(data_dir)
-num_labels = len(labels)
-cache_dir = os.path.join(str(PYTORCH_PRETRAINED_BERT_CACHE), 'distributed_{}'.format(-1))
-model = BertForSequenceClassification.from_pretrained(bert_model,cache_dir=cache_dir,
-              num_labels=num_labels)
+    num_train_optimization_steps = int(len(train_examples)*num_train_epochs)
 
-output_mode = 'classification'
-max_seq_length = 128
-learning_rate = 5e-5
-warmup_proportion = 0.1
-num_train_epochs = 6
-num_train_optimization_steps = int(len(train_examples)*num_train_epochs)
+    train_features = fb.convert_examples_to_features(train_examples, labels, max_seq_length, tokenizer, output_mode)
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+    all_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long)
+    all_label_ids = torch.tensor([f.label_id for f in train_features], dtype=torch.long)
+    train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
 
-train_features = convert_examples_to_features(train_examples, labels, max_seq_length, tokenizer, output_mode)
-param_optimizer = list(model.named_parameters())
-no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-optimizer_grouped_parameters = [
-    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-    ]
-all_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long)
-all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long)
-all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long)
-all_label_ids = torch.tensor([f.label_id for f in train_features], dtype=torch.long)
-train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=8)
+    optimizer = BertAdam(optimizer_grouped_parameters,
+                                 lr=learning_rate,
+                                 warmup=warmup_proportion,
+                                 t_total=num_train_optimization_steps)
 
-train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=8)
-optimizer = BertAdam(optimizer_grouped_parameters,
-                             lr=learning_rate,
-                             warmup=warmup_proportion,
-                             t_total=num_train_optimization_steps)
-
-global_step = 0
-nb_tr_steps = 0
-tr_loss = 0
-
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-model.to(device)
-model.train()
-for _ in trange(int(num_train_epochs), desc="Epoch"):
+    global_step = 0
+    nb_tr_steps = 0
     tr_loss = 0
-    nb_tr_examples, nb_tr_steps = 0, 0
-    for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
-        batch = tuple(t.to(device) for t in batch)
-        input_ids, input_mask, segment_ids, label_ids = batch
 
-        # define a new function to compute loss values for both output_modes
-        logits = model(input_ids, segment_ids, input_mask, labels=None)
-        loss_fct = CrossEntropyLoss()
-        loss = loss_fct(logits.view(-1, num_labels), label_ids.view(-1))
-        loss.backward()
-        tr_loss += loss.item()
-        nb_tr_examples += input_ids.size(0)
-        nb_tr_steps += 1
-        optimizer.step()
-        optimizer.zero_grad()
-        global_step += 1
+    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.train()
+    for _ in trange(int(num_train_epochs), desc="Epoch"):
+        tr_loss = 0
+        nb_tr_examples, nb_tr_steps = 0, 0
+        for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
+            batch = tuple(t.to(device) for t in batch)
+            input_ids, input_mask, segment_ids, label_ids = batch
 
-model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
+            # define a new function to compute loss values for both output_modes
+            logits = model(input_ids, segment_ids, input_mask, labels=None)
+            if output_mode == "classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, num_labels), label_ids.view(-1))
+            elif output_mode == "regression":
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), label_ids.view(-1))
 
-# If we save using the predefined names, we can load using `from_pretrained`
-output_model_file = os.path.join(output_dir, WEIGHTS_NAME)
-output_config_file = os.path.join(output_dir, CONFIG_NAME)
+            loss.backward()
+            tr_loss += loss.item()
+            nb_tr_examples += input_ids.size(0)
+            nb_tr_steps += 1
+            optimizer.step()
+            optimizer.zero_grad()
+            global_step += 1
 
-torch.save(model_to_save.state_dict(), output_model_file)
-model_to_save.config.to_json_file(output_config_file)
-tokenizer.save_vocabulary(output_dir)
+    model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
 
-model = BertForSequenceClassification.from_pretrained(output_dir, num_labels=num_labels)
-tokenizer = BertTokenizer.from_pretrained(output_dir, do_lower_case=True)
+    # If we save using the predefined names, we can load using `from_pretrained`
+    output_model_file = os.path.join(output_dir, WEIGHTS_NAME)
+    output_config_file = os.path.join(output_dir, CONFIG_NAME)
 
-model.to(device)
+    torch.save(model_to_save.state_dict(), output_model_file)
+    model_to_save.config.to_json_file(output_config_file)
+    tokenizer.save_vocabulary(output_dir)
 
-eval_examples = processor.get_dev_examples(data_dir)
-eval_features = convert_examples_to_features(
-    eval_examples, labels, max_seq_length, tokenizer, output_mode)
+    model = BertForSequenceClassification.from_pretrained(output_dir, num_labels=num_labels)
+    tokenizer = BertTokenizer.from_pretrained(output_dir, do_lower_case=True)
+
+    model.to(device)
+
+    eval_examples = processor.get_dev_examples(data_dir,code)
+    eval_features = convert_examples_to_features(
+        eval_examples, labels, max_seq_length, tokenizer, output_mode)
 
 
-all_input_ids = torch.tensor([f.input_ids for f in eval_features], dtype=torch.long)
-all_input_mask = torch.tensor([f.input_mask for f in eval_features], dtype=torch.long)
-all_segment_ids = torch.tensor([f.segment_ids for f in eval_features], dtype=torch.long)
-all_label_ids = torch.tensor([f.label_id for f in eval_features], dtype=torch.long)
+    all_input_ids = torch.tensor([f.input_ids for f in eval_features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in eval_features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in eval_features], dtype=torch.long)
+    if output_mode == "classification":
+        all_label_ids = torch.tensor([f.label_id for f in eval_features], dtype=torch.long)
+    elif output_mode == "regression":
+        all_label_ids = torch.tensor([f.label_id for f in eval_features], dtype=torch.float)
 
-eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
-# Run prediction for full data
-eval_sampler = SequentialSampler(eval_data)
-eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=8)
+    eval_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+    # Run prediction for full data
+    eval_sampler = SequentialSampler(eval_data)
+    eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=8)
 
-model.eval()
-eval_loss = 0
-nb_eval_steps = 0
-preds = []
+    model.eval()
+    eval_loss = 0
+    nb_eval_steps = 0
+    preds = []
 
-for input_ids, input_mask, segment_ids, label_ids in tqdm(eval_dataloader, desc="Evaluating"):
-    input_ids = input_ids.to(device)
-    input_mask = input_mask.to(device)
-    segment_ids = segment_ids.to(device)
-    label_ids = label_ids.to(device)
+    for input_ids, input_mask, segment_ids, label_ids in tqdm(eval_dataloader, desc="Evaluating"):
+        input_ids = input_ids.to(device)
+        input_mask = input_mask.to(device)
+        segment_ids = segment_ids.to(device)
+        label_ids = label_ids.to(device)
 
-    with torch.no_grad():
-        logits = model(input_ids, segment_ids, input_mask, labels=None)
+        with torch.no_grad():
+            logits = model(input_ids, segment_ids, input_mask, labels=None)
 
-    loss_fct = CrossEntropyLoss()
-    tmp_eval_loss = loss_fct(logits.view(-1, num_labels), label_ids.view(-1))
+        if output_mode == "classification":
+            loss_fct = CrossEntropyLoss()
+            tmp_eval_loss = loss_fct(logits.view(-1, num_labels), label_ids.view(-1))
+        elif output_mode == "regression":
+            loss_fct = MSELoss()
+            tmp_eval_loss = loss_fct(logits.view(-1), label_ids.view(-1))
 
-    eval_loss += tmp_eval_loss.mean().item()
-    nb_eval_steps += 1
-    if len(preds) == 0:
-        preds.append(logits.detach().cpu().numpy())
-    else:
-        preds[0] = np.append(
-            preds[0], logits.detach().cpu().numpy(), axis=0)
+        eval_loss += tmp_eval_loss.mean().item()
+        nb_eval_steps += 1
+        if len(preds) == 0:
+            preds.append(logits.detach().cpu().numpy())
+        else:
+            preds[0] = np.append(
+                preds[0], logits.detach().cpu().numpy(), axis=0)
 
-eval_loss = eval_loss / nb_eval_steps
-preds = preds[0]
-preds = np.argmax(preds, axis=1)
-df = pd.read_csv('/home/joseivm/dot-nlp/data/1977/dev.csv',header=None)
-df.iloc[:,2] = preds
-df.columns = ['Code','Title','Prediction']
-df[['Title','Code','Prediction']].to_csv('/home/joseivm/dot-nlp/output/preds.csv')
-result = compute_metrics(preds, all_label_ids.numpy())
-result['eval_loss'] = eval_loss
-output_eval_file = os.path.join(output_dir, "eval_results.txt")
+    eval_loss = eval_loss / nb_eval_steps
+    preds = preds[0]
+    if output_mode == "classification":
+        preds = np.argmax(preds, axis=1)
+    elif output_mode == "regression":
+        preds = np.squeeze(preds)
+        preds = np.rint(preds)
+    df = pd.Series(preds)
+    df.to_csv(os.path.join(results_dir,'preds.csv'),index=False)
+    df.columns = ['Code','Title','Prediction']
+    df[['Title','Code','Prediction']].to_csv('/home/joseivm/dot-nlp/output/preds.csv')
+    result = compute_metrics(preds, all_label_ids.numpy())
+    result['eval_loss'] = eval_loss
+    output_eval_file = os.path.join(output_dir, "eval_results.txt")
 
-with open(output_eval_file, "w") as writer:
-    for key in sorted(result.keys()):
-        writer.write("%s = %s\n" % (key, str(result[key])))
+    with open(output_eval_file, "w") as writer:
+        for key in sorted(result.keys()):
+            writer.write("%s = %s\n" % (key, str(result[key])))
+
+def stitch_predictions():
+    df = pd.read_csv('/home/joseivm/dot-nlp/data/1977/dev.csv',header=None)
+    df.columns = ['Code','Title','Description']
+    codes = ['data','people','things']
+    for code in codes:
+        pred_path = os.path.join(results_dir,code,'preds.csv')
+        code_preds = pd.read_csv(pred_path,header=None)[0]
+        df.loc[:,code] = code_preds
+
+    df.drop(columns='Description',inplace=True)
+    df['DPT'] = df['data'].map(str)+df['people'].map(str)+df['things'].map(str)
+    df.to_csv('/home/joseivm/dot-nlp/results/separate_regression/preds.csv')
+
+train_model('data')
+# train_model('people')
+# train_model('things')
